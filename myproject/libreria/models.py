93e4509df7f96f7622f38cec9bea71389b062525
Jesus peña <jenrique.ps@gmail.com>
2014-07-27 13:46:55 -0500

    implementando models.py libreria

--- myproject/libreria/models.py
+++ myproject/libreria/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    website = models.URLField(blank=True)
+
+
+class Author(models.Model):
+    salutation = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    email = models.EmailField()
+    #headshot = models.ImageField(upload_to='media')
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=140)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
