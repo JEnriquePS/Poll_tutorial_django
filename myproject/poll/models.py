1fae1f2528c1a39b8e64b9130397ef8f1b92a62c
Jesus peña <jenrique.ps@gmail.com>
2014-07-26 22:57:58 -0500

    registrando modelos en admin.py

--- myproject/poll/models.py
+++ myproject/poll/models.py
@@ -5,6 +5,7 @@ class Poll(models.Model):
     pregunta = models.CharField(max_length=140)
     pub_date =models.DateTimeField('Tiempo de Publicacion')
 
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     opcion = models.CharField(max_length=50)
