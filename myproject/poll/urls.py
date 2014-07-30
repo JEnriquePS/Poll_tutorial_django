e8526be144efc688cb66b84b0357938f5421139a
Jesus peña <jenrique.ps@gmail.com>
2014-07-30 16:58:58 -0500

    views.py de app poll terminada

--- myproject/poll/urls.py
+++ myproject/poll/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('poll.views',
                        url(r'^poll/$', 'poll_vista',name='detail2'),
                        url(r'^(?P<pk>\d+)/$', 'detalle_poll',
                         name='detalle_poll'),
+                       url(r'^(?P<pk>\d+)/$', 'detalle_choice',
+                        name='detalle_choice')
 )
--- myproject/poll/views.py
+++ myproject/poll/views.py
@@ -16,11 +16,17 @@ def incrementar_tiempo(request, aumento):
     html = "<html><body>esta es mi hora %s </body></html>" % dt
     return HttpResponse(html)
 
+
 def poll_vista(request):
-    pregunta = Poll.objects.all().order_by('-pub_date')
+    pregunta = Poll.objects.all().order_by('-pub_date')[:5]
     context = {'poll_lista_pregunta':pregunta}
     return render(request,'home.html', context)
+
 
 def detalle_poll(request, pk):
     pre = get_object_or_404(Poll, pk=pk)
     return render(request, 'detail.html', {'pregunta':pre})
+
+def detalle_choice(request, choice_id):
+    choice_text = get_object_or_404(Choice, pk = choice_id)
+    return render(request, 'choice_detalle.html', {'choice_id':choice_id})
