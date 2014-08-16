a850123535985b5924740008952fc225b68510ed
Jesus peña <jenrique.ps@gmail.com>
2014-08-15 23:06:14 -0500

    Documentando README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+>El objetivo de este documento es brindar las especificaciones técnicas suficientes para entender cómo funciona Django,
+pero no ser un tutorial o una referencia – ambos existen! Cuando estés listo para empezar un proyecto, podés chequear
+el tutorial o sumergirte en la documentación más detallada.
--- myproject/poll/urls.py
+++ myproject/poll/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('poll.views',
-                       url(r'^time/$', 'tiempo_actual', name='detail1'),
-                       url(r'^add_time/(\d{1,2})/$', 'incrementar_tiempo', name='detail2'),
-                       url(r'^poll/$', 'index',name='index'),
-                       url(r'^poll/(?P<poll_id>\d+)/$', 'detail', name='detail'),
-                       url(r'^(?P<poll_id>\d+)/vote/$', 'vote', name='vote'),
-                       url(r'^(?P<poll_id>\d+)/results/$', 'results', name='results'),
+    url(r'^time/$', 'tiempo_actual', name='detail1'),
+    url(r'^add_time/(\d{1,2})/$', 'incrementar_tiempo', name='detail2'),
+    url(r'^poll/$', 'index', name='index'),
+    url(r'^poll/(?P<poll_id>\d+)/$', 'detail', name='detail'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote', name='vote'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results', name='results'),
 )
